c76ade107191a59e0d55d5d33971fd01c987eedb
Gustavo <140917585+Guzitos@users.noreply.github.com>
2025-12-28 17:12:53 -0300

    base

---
+++ demo/src/main/java/com/agents/demo/KairuNativeCloneApplication.java
@@ -0,0 +1,13 @@
+package com.agents.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KairuNativeCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KairuNativeCloneApplication.class, args);
+	}
+
+}
