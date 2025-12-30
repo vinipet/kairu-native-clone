027a7ce6bf74aeec54d51dd65d9b90b4f2124cba
Gustavo <140917585+Guzitos@users.noreply.github.com>
2025-12-30 14:07:48 -0300

    update in Entity Project

--- src/main/java/com/agents/demo/dto/project/ProjectRequestDTO.java
+++ src/main/java/com/agents/demo/dto/project/ProjectRequestDTO.java
@@ -1,19 +1,28 @@
 package com.agents.demo.dto.project;
 
+import com.agents.demo.entity.Project;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
-@Getter
-@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class ProjectRequestDTO {
 
     @NotBlank
     private String name;
 
     private String description;
+    private String status;
+    private LocalDate deadline;
 
-    private LocalDateTime deadline;
+    public ProjectRequestDTO(Project project) {
+        this.name = project.getName();
+        this.description = project.getDescription();
+        this.status = project.getStatus().name(); // enum → String
+        this.deadline = LocalDate.from(project.getDeadline());
+    }
+
 }
--- src/main/java/com/agents/demo/dto/project/ProjectResponseDTO.java
+++ src/main/java/com/agents/demo/dto/project/ProjectResponseDTO.java
@@ -1,20 +1,29 @@
 package com.agents.demo.dto.project;
 
+import com.agents.demo.entity.Project;
 import com.agents.demo.enums.ProjectStatus;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class ProjectResponseDTO {
 
-    private Long id;
+    private int id;
     private String name;
     private String description;
     private ProjectStatus status;
     private LocalDateTime createdAt;
     private LocalDateTime deadline;
 
+    public ProjectResponseDTO(Project project) {
+        this.id = project.getId();
+        this.name = project.getName();
+        this.description = project.getDescription();
+        this.status = ProjectStatus.valueOf(project.getStatus().name());
+        this.deadline = project.getDeadline();
+        this.createdAt = project.getCreatedAt();
+    }
 }
