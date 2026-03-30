7e6baedf66b78f6ff0bfeb3313f036072b2ae5cf
vinipet <viniciuspet123@gmail.com>
2026-03-30 19:53:16 -0300

    some test for the bus

--- src/test/java/com/kairu/core/bus/SimpleEventBusTest.java
+++ src/test/java/com/kairu/core/bus/SimpleEventBusTest.java
@@ -9,6 +9,12 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimpleEventBusTest {
+  
+    public class ExeptionTest extends RuntimeException {
+        public ExeptionTest(String message) {
+          super(message);
+        }
+    }
 
     static class TestEvent extends BaseEvent {
 
@@ -21,10 +27,10 @@ class SimpleEventBusTest {
     void listenerReceivesPublishedEvent() {
         SimpleEventBus bus = new SimpleEventBus();
         AtomicBoolean received = new AtomicBoolean(false);
-        bus.subscribe(TestEvent.class, event -> {
+        bus.subscribeListener(TestEvent.class, event -> {
             received.set(true);
         });
-        bus.publish(new TestEvent(Instant.now()));
+        bus.publishEvent(new TestEvent(Instant.now()));
         assertTrue(received.get());
     }
 
@@ -32,7 +38,7 @@ class SimpleEventBusTest {
     void publishingEventWithNoListenersDoesNotCrash() {
       SimpleEventBus bus = new SimpleEventBus();
       TestEvent event = new TestEvent(Instant.now());
-      assertDoesNotThrow(() -> bus.publish(event));
+      assertDoesNotThrow(() -> bus.publishEvent(event));
     }
     static class AnotherEvent extends BaseEvent {
 
@@ -41,15 +47,63 @@ class SimpleEventBusTest {
       }
     }
 
-  @Test
-  void listenerOnlyReceivesSubscribedEventType() {
-    SimpleEventBus bus = new SimpleEventBus();
-    AtomicBoolean received = new AtomicBoolean(false);
-    bus.subscribe(TestEvent.class, event -> {
-        received.set(true);
-    });
-    bus.publish(new AnotherEvent(Instant.now()));
-    assertFalse(received.get());
-  }
+    @Test
+    void listenerOnlyReceivesSubscribedEventType() {
+      SimpleEventBus bus = new SimpleEventBus();
+      AtomicBoolean received = new AtomicBoolean(false);
+      bus.subscribeListener(TestEvent.class, event -> {
+         received.set(true);
+      });
+      bus.publishEvent(new AnotherEvent(Instant.now()));
+      assertFalse(received.get());
+    }
 
+    @Test 
+    void twoListenerReceiveSameEvent() {
+        SimpleEventBus bus = new SimpleEventBus();
+
+        AtomicBoolean firstListenerExecuted = new AtomicBoolean(false);
+        AtomicBoolean secondListenerExecuted = new AtomicBoolean(false);
+
+        bus.subscribeListener(TestEvent.class, event -> {
+          firstListenerExecuted.set(true);
+        });
+
+        bus.subscribeListener(TestEvent.class, event -> {
+          secondListenerExecuted.set(true);
+        });
+
+        bus.publishEvent(new TestEvent(Instant.now()));
+
+        assertTrue(firstListenerExecuted.get());
+        assertTrue(secondListenerExecuted.get());
+    }
+    
+    @Test
+    void shouldContinueProcessingWhenListenerThrowsException() {
+      SimpleEventBus bus = new SimpleEventBus();
+      AtomicBoolean firstreceived = new AtomicBoolean(false);
+      AtomicBoolean secondreceived = new AtomicBoolean(false);
+      AtomicBoolean thirdreceived = new AtomicBoolean(false);
+
+      bus.subscribeListener(TestEvent.class, event -> {
+        firstreceived.set(true);
+      });
+        
+      bus.subscribeListener(TestEvent.class, event -> {
+        secondreceived.set(true);
+        throw new ExeptionTest("disparou o erro em fio");  
+      });
+      
+      bus.subscribeListener(TestEvent.class, event -> {
+        thirdreceived.set(true); 
+      });
+
+      assertDoesNotThrow(()-> {
+        bus.publishEvent(new TestEvent(Instant.now()));
+      });
+      assertTrue(firstreceived.get());
+      assertTrue(secondreceived.get());
+      assertTrue(thirdreceived.get());
+  }
 }
