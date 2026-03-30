8762bd5fafff4148c923608c7ce22efe117c1196
vinipet <viniciuspet123@gmail.com>
2026-03-30 19:54:35 -0300

    more tests

--- src/main/java/com/kairu/core/bus/SimpleEventBus.java
+++ src/main/java/com/kairu/core/bus/SimpleEventBus.java
@@ -8,19 +8,23 @@ public class SimpleEventBus implements EventBus {
 
     @Override
     @SuppressWarnings("unchecked")
-    public <T extends Event> void publish(T event) {
+    public <T extends Event> void publishEvent(T event) {
         Class<? extends Event> eventType = event.getClass();
 
         List <EventListener<? extends Event>> eventListeners =
                 listeners.getOrDefault(eventType, List.of());
 
         for (EventListener <? extends Event> listener : eventListeners) {
-            ((EventListener<T>) listener).onEvent(event);
+              try {
+                ((EventListener<T>) listener).onEvent(event);
+              } catch (Exception e) {
+                System.err.println("listener falhou aqui" + e.getMessage());
+              }
         }
     }
 
     @Override
-    public <T extends Event> void subscribe(
+    public <T extends Event> void subscribeListener(
             Class<T> eventType,
             EventListener<T> listener
     ) {
