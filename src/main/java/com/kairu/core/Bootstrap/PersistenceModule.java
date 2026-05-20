5aae1edb95babc998e8a4168ddac33b42c67d4a1
vinipet <viniciuspet123@gmail.com>
2026-05-20 20:36:50 -0300

    adding a tag creator metods in percistence module

--- src/main/java/com/kairu/core/Bootstrap/PersistenceModule.java
+++ src/main/java/com/kairu/core/Bootstrap/PersistenceModule.java
@@ -4,17 +4,28 @@ import java.nio.file.Path;
 
 import com.kairu.core.persistence.GsonFactory;
 import com.kairu.core.persistence.repository.FileSessionRepository;
+import com.kairu.core.persistence.repository.FileTagRepository;
 import com.kairu.core.session.InMemorySessionRepository;
+import com.kairu.core.session.InMemoryTagRepository;
 import com.kairu.core.session.SessionRepository;
+import com.kairu.core.session.TagRepository;
 
 public class PersistenceModule{
 
 
-  public static SessionRepository initializeFilePersistence(Path path){
+  public static SessionRepository initializeFileSessionPersistence(Path path){
     return new FileSessionRepository(path, GsonFactory.create());
   }
 
-  public static SessionRepository initializeInMemoryPercistence(){
+  public static SessionRepository initializeInMemorySessionPercistence(){
     return new InMemorySessionRepository();
+  }
+
+  public static TagRepository initializeInMemoryTagPersistence(){
+    return new InMemoryTagRepository();
+  }
+
+  public static TagRepository initializeFileTagPersistence(Path path){
+    return new FileTagRepository(path, GsonFactory.create());
   }
 }
