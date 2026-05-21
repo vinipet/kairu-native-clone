db5a5b719c2c9da6ce2c13918cf0a977d2ea150b
vinipet <viniciuspet123@gmail.com>
2026-05-21 02:59:41 -0300

    now core module have a ijected clock
    for better tests

--- src/main/java/com/kairu/core/Bootstrap/CoreModule.java
+++ src/main/java/com/kairu/core/Bootstrap/CoreModule.java
@@ -3,14 +3,12 @@ package com.kairu.core.Bootstrap;
 import com.kairu.core.bus.EventBus;
 import com.kairu.core.session.SessionManager;
 import com.kairu.core.time.Clock;
-import com.kairu.core.time.RealClock;
 import com.kairu.core.time.TimerFactory;
 
 public class CoreModule{
 
 
-  public  static SessionManager createCore(EventBus bus){
-    Clock clock = new RealClock();
+  public  static SessionManager createCore(EventBus bus, Clock clock){
     TimerFactory factory = new TimerFactory(clock, bus);
     SessionManager manager = new SessionManager(bus, clock, factory);
     return manager;
