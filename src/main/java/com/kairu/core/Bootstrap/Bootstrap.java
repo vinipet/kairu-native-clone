c0de6816b7ffb9c9a541c5c3a593bd979c161ca7
vinipet <viniciuspet123@gmail.com>
2026-05-21 02:14:25 -0300

    now clock will injected on bootstrap, not created on then

--- src/main/java/com/kairu/core/Bootstrap/Bootstrap.java
+++ src/main/java/com/kairu/core/Bootstrap/Bootstrap.java
@@ -1,5 +1,6 @@
 package com.kairu.core.Bootstrap;
 
+
 import com.kairu.core.bus.EventBus;
 import com.kairu.core.bus.SimpleEventBus;
 import com.kairu.core.event.SessionCompletedEvent;
@@ -8,13 +9,14 @@ import com.kairu.core.session.SessionCompletedPersistenceListener;
 import com.kairu.core.session.SessionManager;
 import com.kairu.core.session.SessionRepository;
 import com.kairu.core.session.TagRepository;
+import com.kairu.core.time.Clock;
 
 public class Bootstrap{
   
-  public static ApplicationContext createFileContext(){
+  public static ApplicationContext createFileContext(Clock clock){
     EventBus bus = new SimpleEventBus();
     SessionRepository repository =  PersistenceModule.initializeFileSessionPersistence(PersistencePaths.sessionsFile());
-    SessionManager manager = CoreModule.createCore(bus);
+    SessionManager manager = CoreModule.createCore(bus, clock);
     TagRepository tagRepository = PersistenceModule.initializeFileTagPersistence(PersistencePaths.TagFile());
 
     bus.subscribeListener(SessionCompletedEvent.class, new SessionCompletedPersistenceListener(repository));
@@ -23,10 +25,10 @@ public class Bootstrap{
   }
 
 
-  public static ApplicationContext createInMemoryContext(){
+  public static ApplicationContext createInMemoryContext(Clock clock){
     EventBus bus = new SimpleEventBus();
     SessionRepository repository =  PersistenceModule.initializeInMemorySessionPercistence();
-    SessionManager manager = CoreModule.createCore(bus);
+    SessionManager manager = CoreModule.createCore(bus, clock);
     TagRepository tagRepository = PersistenceModule.initializeInMemoryTagPersistence();
 
     bus.subscribeListener(SessionCompletedEvent.class, new SessionCompletedPersistenceListener(repository));
